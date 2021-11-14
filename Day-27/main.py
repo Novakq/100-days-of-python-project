7ffc58ad09ad98e9a8b72e1eca5245b7957d0560
Filip Nowakowski <filipnowakowskimail@gmail.com>
2021-11-14 18:33:17 +0100

    day 27 done

--- Day-27/main.py
+++ Day-27/main.py
@@ -2,32 +2,38 @@ from tkinter import *
 
 
 def button_clicked():
-    print("I got clicked")
     new_text = input.get()
-    my_label.config(text=new_text)
+    result.config(text=float(new_text)*1.609344)
 
 
 window = Tk()
-window.title("My First GUI Program")
+window.title("Miles to KM converter")
 window.minsize(width=500, height=300)
 window.config(padx=100, pady=200)
 
 #Label
-my_label = Label(text="I Am a Label", font=("Arial", 24, "bold"))
-my_label.config(text="New Text")
+my_label = Label(text="I Am a Label", font=("Arial", 18, "bold"))
+my_label.config(text="Please input miles")
 my_label.grid(column=0, row=0)
 my_label.config(padx=50, pady=50)
 
-#Button
-button = Button(text="Click Me", command=button_clicked)
-button.grid(column=1, row=1)
+my_label_2 = Label(text="I Am a Label", font=("Arial", 18, "bold"))
+my_label_2.config(text="Result")
+my_label_2.grid(column=0, row=1)
+my_label_2.config(padx=50, pady=50)
 
-new_button = Button(text="New Button")
-new_button.grid(column=2, row=0)
+#Label KM
+result = Label(text="I Am a Label", font=("Arial", 12, "bold"))
+result.grid(column=1, row=1)
+result.config(padx=50, pady=50)
+
+#Button
+button = Button(text="Convert to KM", command=button_clicked)
+button.grid(column=2, row=0)
 
 #Entry
 input = Entry(width=10)
 print(input.get())
-input.grid(column=3, row=2)
+input.grid(column=1, row=0)
 
 window.mainloop()
